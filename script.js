5b815989854f883aaaa49f71255dc3ee2fc3c38e
Syedmominshah <shahsyedmomin93@gmail.com>
2023-10-22 00:44:27 -0700

    light

---
+++ script.js
@@ -0,0 +1,11 @@
+gsap.from( '#five,#numbergroup path',{
+    stagger: {
+        each:0.05,
+    },
+    scaley:0,
+    delay:0.6,
+    duration:0.8,
+    transformOrigin:'50% 100%',
+    ease:'elastic(0.73, 0.6)',
+
+});
